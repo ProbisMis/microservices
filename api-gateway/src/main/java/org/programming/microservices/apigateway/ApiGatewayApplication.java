aafc5209ce8bd0e2cbf933184c861758495b5012
ProbisMis <buraksmeric@gmail.com>
2026-01-16 15:39:07 +0300

    fix gateway errors

--- api-gateway/src/main/java/org/programming/microservices/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/org/programming/microservices/apigateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package org.programming.microservices.apigateway;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- api-gateway/src/main/java/org/programming/microservices/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/org/programming/microservices/apigateway/routes/Routes.java
@@ -1,34 +1,38 @@
 package org.programming.microservices.apigateway.routes;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+// Gateway MVC specific imports
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 
-@COnfiguration
+@Configuration
 public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute() {
-        return GatewayRouterFunctions
-            .route("product-service")
-            .route(RequestPredicates.path("/api/product"), 
-                HandlerFunctions.http("http://localhost:8080")).build();
+        return GatewayRouterFunctions.route("product-service")
+                .route(RequestPredicates.path("/api/product"), 
+                        HandlerFunctions.http("http://localhost:8080"))
+                .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoute() {
-        return GatewayRouterFunctions
-            .route("order-service")
-            .route(RequestPredicates.path("/api/order"), 
-                HandlerFunctions.http("http://localhost:8081")).build();
+        return GatewayRouterFunctions.route("order-service")
+                .route(RequestPredicates.path("/api/order"), 
+                        HandlerFunctions.http("http://localhost:8081"))
+                .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceRoute() {
-        return GatewayRouterFunctions
-            .route("inventory-service")
-            .route(RequestPredicates.path("/api/inventory"), 
-                HandlerFunctions.http("http://localhost:8082")).build();
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(RequestPredicates.path("/api/inventory"), 
+                        HandlerFunctions.http("http://localhost:8082"))
+                .build();
     }
 }
